3df1591543a73bfc747d4cd4e253dcf1f0aa640e
ianmateusES <estudo.ianmateus@gmail.com>
2023-08-31 01:46:10 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { transactionsRoutes } from './routes/transactions'
+
+export const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
